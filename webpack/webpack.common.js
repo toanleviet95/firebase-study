d9bea1076ccc07352ba930fc301024b069c2e2fa
Toan Le Viet <toanleviet95@gmail.com>
2019-09-08 00:54:32 +0700

    Init project

---
+++ webpack/webpack.common.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve('dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+};
