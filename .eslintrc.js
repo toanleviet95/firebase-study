4be78019689dcf3f7e552eee836c2897a155c4eb
Toan Le Viet <toanleviet95@gmail.com>
2019-09-08 02:27:11 +0700

    Add eslint-loader

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,6 @@ module.exports = {
     browser: true,
     node: true,
     es6: true,
-    mocha: true,
-    jest: true,
-    jasmine: true,
-  },
-  globals: {
-    APP_TYPE: true,
   },
   plugins: [
     'react-hooks',
@@ -21,6 +15,7 @@ module.exports = {
     'react-hooks/exhaustive-deps': 1,
     'comma-dangle': [1, 'always-multiline'],
     'eol-last': [2, 'always'],
+    'import/no-unresolved': [2, { ignore: ['^@/'] }],
     quotes: [1, 'single'],
     semi: [1, 'always'],
     'jsx-quotes': [1, 'prefer-single'],
